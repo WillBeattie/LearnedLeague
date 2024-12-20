38ef2036289b6c5a7d9405205e5eb929080fcea5
Will Beattie <willjbeattie@gmail.com>
2024-12-20 13:55:08 -0500

    Update for Season 103

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def index(user_input=None):
 if __name__ == '__main__':
     port = int(environ.get("PORT", 5000))  # Use the port from environment variable, default to 5000
     app.run(host='0.0.0.0', port=port)
-    # app.run(debug=True)
+    #app.run(debug=True)
